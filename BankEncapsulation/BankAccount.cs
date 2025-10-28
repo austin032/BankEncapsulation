863ad7b4aba75267eb312c0776eaac8165bd0421
Austin Okyere <okyereaustin@gmail.com>
2025-10-28 15:59:25 -0600

    Completed Encapsulation exercise

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace BankEncapsulation;
+
+public class BankAccount
+{
+    private double _balance;
+
+    public void Deposit(double amount)
+    {
+        _balance = amount;
+    }
+
+    public double GetBalance()
+    {
+        return _balance;
+    }
+    
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var ba =  new BankAccount();
+            
+            Console.WriteLine("How much money would you like to deposit?");
+            var amountToDeposit = double.Parse(Console.ReadLine());
+            
+            ba.Deposit(amountToDeposit);
+            var userBalance = ba.GetBalance();
+            
+            Console.WriteLine($"Your current balance is: {userBalance, 0:c}");
         }
     }
 }
